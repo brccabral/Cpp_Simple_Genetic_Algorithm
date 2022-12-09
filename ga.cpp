e611d6d6895d54ba8356b82e7535b85182b8e5ac
Bruno Cabral <cabral.brc@gmail.com>
2022-12-09 13:31:44 -0800

    reduce the NUM of objects, and loop

--- ga.cpp
+++ ga.cpp
@@ -23,52 +23,55 @@ int main()
     std::uniform_real_distribution<double> unif(-100, 100);
     std::vector<Solution> solutions;
 
-    const int NUM = 9000000;
+    const int NUM = 100000;
     for (int i = 0; i < NUM; i++)
     {
         solutions.push_back(Solution{0, unif(device), unif(device), unif(device)});
     }
 
-    // run our fitness function
-    for (auto &s : solutions)
+    while (true)
     {
-        s.fitness();
-    }
+        // run our fitness function
+        for (auto &s : solutions)
+        {
+            s.fitness();
+        }
 
-    // sort our solutions by rank
-    std::sort(solutions.begin(), solutions.end(), [](const auto &left, const auto &right)
-              { return left.rank > right.rank; });
+        // sort our solutions by rank
+        std::sort(solutions.begin(), solutions.end(), [](const auto &left, const auto &right)
+                  { return left.rank > right.rank; });
 
-    // print top solutions
-    std::for_each(solutions.begin(), solutions.begin() + 10, [](const auto &s)
-                  { std::cout << std::fixed
-                              << "Rank " << static_cast<int>(s.rank)
-                              << "\nx=" << s.x << "\ny=" << s.y << "\nz=" << s.z << "\n"
-                              << std::endl; });
+        // print top solutions
+        std::for_each(solutions.begin(), solutions.begin() + 10, [](const auto &s)
+                      { std::cout << std::fixed
+                                  << "Rank " << static_cast<int>(s.rank)
+                                  << "\nx=" << s.x << "\ny=" << s.y << "\nz=" << s.z << "\n"
+                                  << std::endl; });
 
-    // take top solutions
-    const int SAMPLE_SIZE = 1000;
-    std::vector<Solution> sample;
-    std::copy(solutions.begin(), solutions.begin() + SAMPLE_SIZE, std::back_inserter(sample));
-    solutions.clear();
+        // take top solutions
+        const int SAMPLE_SIZE = 1000;
+        std::vector<Solution> sample;
+        std::copy(solutions.begin(), solutions.begin() + SAMPLE_SIZE, std::back_inserter(sample));
+        solutions.clear();
 
-    // mutate the top solutions by 1%
-    std::uniform_real_distribution<double> m(0.99, 1.01);
-    std::for_each(sample.begin(), sample.end(), [&](auto &s)
-                  {
-        s.x *= m(device);
-        s.y *= m(device);
-        s.z *= m(device); });
+        // mutate the top solutions by 1%
+        std::uniform_real_distribution<double> m(0.99, 1.01);
+        std::for_each(sample.begin(), sample.end(), [&](auto &s)
+                      {
+                        s.x *= m(device);
+                        s.y *= m(device);
+                        s.z *= m(device); });
 
-    // cross over
-    std::uniform_int_distribution<int> cross(0, SAMPLE_SIZE - 1);
-    // mix SAMPLE_SIZE objects to generate NUM amount of new objects
-    for (int i = 0; i < NUM; i++)
-    {
-        solutions.push_back(Solution{
-            0,
-            sample[cross(device)].x,
-            sample[cross(device)].y,
-            sample[cross(device)].z});
+        // cross over
+        std::uniform_int_distribution<int> cross(0, SAMPLE_SIZE - 1);
+        // mix SAMPLE_SIZE objects to generate NUM amount of new objects
+        for (int i = 0; i < NUM; i++)
+        {
+            solutions.push_back(Solution{
+                0,
+                sample[cross(device)].x,
+                sample[cross(device)].y,
+                sample[cross(device)].z});
+        }
     }
 }
